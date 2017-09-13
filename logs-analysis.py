9ff61fd765f54eed071bf787a94cdbb35707b8e9
Henry Siau <siauhenry@gmail.com>
2017-09-13 16:42:28 -0700

    Add shebang line

--- logs-analysis.py
+++ logs-analysis.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import io
 import psycopg2
 
@@ -5,11 +6,11 @@ import psycopg2
 DBNAME = "news"
 # 1. What are the most popular three articles of all time?
 query_1 = """
- select articles.slug, count(*)as views
- from log join articles on
- log.path like concat('%', articles.slug)
- group by articles.slug, log.path
- order by views desc limit 3
+ SELECT articles.slug, COUNT(*) AS views
+ FROM log JOIN articles on
+ log.path LIKE CONCAT('%', articles.slug)
+ GROUP BY articles.slug, log.path
+ ORDER BY views DESC limit 3
  """
 
 db = psycopg2.connect(dbname=DBNAME)
@@ -18,13 +19,15 @@ c.execute(query_1)
 raw_result = c.fetchall()
 f = open('results.txt', 'ab')
 f.write("The most popular three articles of all time.\n")
+print("The most popular three articles of all time.")
 for item in raw_result:
     title = str(
         item[0].replace("-", " ")).title()
     views = int(item[1])
-    f.write(
-        title + " --" + str(views) + " Views\n")
+    f.write(title + " --" + str(views) + " Views\n")
+    print(title + " --" + str(views) + " Views")
 f.write("\n")
+print("")
 c.close()
 f.close()
 
@@ -33,18 +36,18 @@ f.close()
 # with the most popular author at the top.
 
 query_2 = """
- select
- authors.name, sum(sub.views) as sum_views
- from
- (select articles.slug, articles.author,
- count(*)as views
- from log join articles
- on log.path like concat('%', articles.slug)
- group by articles.slug, log.path, articles.author
- order by views desc) sub
- join authors on sub.author = authors.id
- group by authors.id, sub.author
- order by sum_views desc
+ SELECT
+ authors.name, sum(sub.views) AS sum_views
+ FROM
+ (SELECT articles.slug, articles.author,
+ COUNT(*)AS views
+ FROM log JOIN articles
+ on log.path LIKE CONCAT('%', articles.slug)
+ GROUP BY articles.slug, log.path, articles.author
+ ) sub
+ JOIN authors ON sub.author = authors.id
+ GROUP BY authors.id, sub.author
+ ORDER BY sum_views DESC
 """
 db = psycopg2.connect(dbname=DBNAME)
 c = db.cursor()
@@ -52,11 +55,12 @@ c.execute(query_2)
 raw_result = c.fetchall()
 f = open('results.txt', 'ab')
 f.write("The most popular article authors of all time.\n")
+print("The most popular article authors of all time.")
 for item in raw_result:
-    f.write(str(item[0]))
-    f.write(" --")
-    f.write(str(item[1]) + " Views\n")
+    f.write(str(item[0]) + " --" + str(item[1]) + " Views\n")
+    print(str(item[0]) + " --" + str(item[1]) + " Views")
 f.write("\n")
+print("")
 c.close()
 f.close()
 
@@ -64,17 +68,17 @@ f.close()
 # of requests lead to errors?
 
 query_3 = """
-select sub_0.date,
-(sub_0.errors::decimal / sub_1.views) as err_rate from
-(select
-time::DATE as date, count(*) as errors from log
+SELECT sub_0.date,
+(sub_0.errors::decimal / sub_1.views) AS err_rate FROM
+(SELECT
+time::DATE AS date, COUNT(*) AS errors FROM log
 where status = '404 NOT FOUND'
-group by time::DATE) as sub_0
-join
-(select time::DATE as date,
-count(*) as views from log group by time::DATE) as sub_1
+GROUP BY time::DATE) AS sub_0
+JOIN
+(SELECT time::DATE AS date,
+COUNT(*) AS views FROM log GROUP BY time::DATE) AS sub_1
 on sub_0.date = sub_1.date
-where
+WHERE
 sub_0.errors  > (0.01 * sub_1.views)
 """
 
@@ -84,12 +88,16 @@ c.execute(query_3)
 raw_result = c.fetchall()
 f = open('results.txt', 'ab')
 f.write("These days did more than 1% of requests lead to errors.\n")
+print("These days did more than 1% of requests lead to errors.")
 for item in raw_result:
-    f.write(str(item[0].strftime("%B %d, %Y")) + " --")
-    f.write(
-        "{0:.2f}". format(float(item[1]) * 100) +
-        "% errors\n")
+    f.write(str(item[0].strftime("%B %d, %Y")) +
+            " --" "{0:.2f}". format(float(item[1]) * 100) +
+            "% errors\n")
+    print(str(item[0].strftime("%B %d, %Y")) +
+          " --" "{0:.2f}". format(float(item[1]) * 100) +
+          "% errors")
 
 f.write("\n")
+print("")
 c.close()
 f.close()
